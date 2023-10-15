1e14a2846aa6d0363da1a53bbb1d8b94eb8145d7
VVoron <vkunievskiy@list.ru>
2023-10-15 15:02:51 +0300

    [lab1/dev] Добавление базовых классов и функционала в них

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Philosopher
+{
+    public int Id;
+    public int TimeForEating;
+    public bool WellFed;
+    public List<Fork> Forks = new List<Fork>();
+
+    // rnd необходим для случайной генерации
+    public Philosopher(int id, List<Fork> forks, Random rnd)
+    {
+        this.Id = id;
+        this.TimeForEating = rnd.Next(2000, 5000);
+        this.WellFed = false;
+        this.Forks = forks;
+    }
+
+    public void Eating()
+    {
+        while (!WellFed)
+        {
+            if (Forks.Any(x => x.InUsage))
+                continue;
+
+            foreach(var fork in Forks)
+                fork.InUsage = true;
+
+            Thread.Sleep(TimeForEating);
+            WellFed = true;
+            Console.WriteLine($"Философ {Id} - поел");
+            foreach(var fork in Forks)
+                fork.InUsage = false;
+        }
+    }
+}
+
+public class Fork
+{
+    public int Id;
+    public bool InUsage;
+
+    public Fork(int id)
+    {
+        this.Id = id;
+        this.InUsage = false;
+    }
+}
